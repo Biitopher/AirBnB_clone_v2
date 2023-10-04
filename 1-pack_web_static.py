26b7d8d6cb97bc522bf7f991896e299af162079a
biitopher <biitopher@gmail.com>
2023-10-04 09:22:59 -0700

    Task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#Using function d0 pack
+
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """Generates a .tgz archive from the web_static folder."""
+    
+    if not os.path.exists('versions'):
+        os.makedirs('versions')
+
+    now = datetime.now()
+    archive_name = 'web_static_{}.tgz'.format(
+        now.strftime('%Y%m%d%H%M%S')
+    )
+
+    tar_command = 'tar -cvzf versions/{} web_static'.format(archive_name)
+    result = local(tar_command)
+
+    if result.succeeded:
+        return 'versions/{}'.format(archive_name)
+    else:
+        return None
