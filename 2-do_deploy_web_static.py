e6909dcfba2dfb312d19f5e436aebfd5d0d17dbe
biitopher <biitopher@gmail.com>
2023-10-04 10:25:23 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 
 
 from fabric.api import *
-import os import exists
+import os path import exists
 from datetime import datetime
 
 
@@ -12,27 +12,28 @@ env.hosts = ['34.229.69.25', '100.26.227.84']
 def do_deploy(archive_path):
     """Distributes an archive to the web servers."""
     
-    if not exists(archive_path):
+    if exists(archive_path) is False:
         return False
+    filename = archive_path.split('/')[-1]
+
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+
+    tmp = "/tmp/" + filename
 
     try:
-        put(archive_path, '/tmp/')
+        put(archive_path, "/tmp/")
 
-        archive_filename = archive_path.split('/')[-1]
-        folder_name = archive_filename.replace('.tgz', '')
-        release_path = '/data/web_static/releases/{}/'.format(folder_name)
-        run('mkdir -p {}'.format(release_path))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
+        run("mkdir -p {}/".format(no_tgz))
 
-        run('rm /tmp/{}'.format(archive_filename))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
 
-        current_link = '/data/web_static/current'
-        run('rm -f {}'.format(current_link))
+        run("rm -rf /data/web_static/current")
 
-        run('ln -s {} {}'.format(release_path, current_link))
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
 
-        print("New version deployed!")
         return True
-
-    except Exception as e:
+    except:
         return False
