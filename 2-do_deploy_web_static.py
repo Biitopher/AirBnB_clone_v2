51492fe11e57cff758eef72c4eeca79ebc427c03
biitopher <biitopher@gmail.com>
2023-10-04 10:08:35 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,22 +15,22 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    try:
+     try:
         put(archive_path, '/tmp/')
-        
-        archive_filename = os.path.basename(archive_path)
-        folder_name = archive_filename[:-4]
-        run('mkdir -p /data/web_static/releases/{}'.format(folder_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            archive_filename, folder_name))
-        
+
+        archive_filename = archive_path.split('/')[-1]
+        folder_name = archive_filename.replace('.tgz', '')
+        release_path = '/data/web_static/releases/{}/'.format(folder_name)
+        run('mkdir -p {}'.format(release_path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
+
         run('rm /tmp/{}'.format(archive_filename))
-        
-        run('rm -f /data/web_static/current')
-        
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'.format(
-            folder_name)) 
-        
+
+        current_link = '/data/web_static/current'
+        run('rm -f {}'.format(current_link))
+
+        run('ln -s {} {}'.format(release_path, current_link))
+
         print("New version deployed!")
         return True
 
