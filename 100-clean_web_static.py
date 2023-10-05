377b71b2eb6e7c845a405174641ac124e545a3c1
biitopher <biitopher@gmail.com>
2023-10-05 14:05:04 -0700

    Task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """Deletes out-of-date archives, using the function do_clean"""
 
-
 from fabric.api import env, run, lcd
 from datetime import datetime
-
+import os
 env.user = 'ubuntu'
 env.hosts = ['34.229.69.25', '100.26.227.84']
 
+
 def do_clean(number=0):
-    number = int(number)
-    if number < 1:
-        number = 1
+     number = 1 if int(number) == 0 else int(number)
 
-    with lcd('versions'):
-        local('ls -t | tail -n +{} | xargs -I {} rm -f {}'.format(
-              number + 1, '{}', '{}'))
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-    with cd('/data/web_static/releases'):
-        run('ls -t | tail -n +{} | xargs -I {} rm -rf {}'.format(
-            number + 1, '{}', '{}'))
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
