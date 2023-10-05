4c53a43035a7748656cb8633f464bb37b0f3c826
biitopher <biitopher@gmail.com>
2023-10-05 10:28:14 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@
 
 
 from fabric.api import *
-import os
+import os import exists
 from datetime import datetime
 
 env.hosts = ['34.229.69.25', '100.26.227.84']
@@ -12,24 +12,40 @@ env.user = 'ubuntu'
 
 def do_deploy(archive_path):
     """Distributes an archive to the web servers."""
-    if not os.path.exists(archive_path):
+    if not exists(archive_path):
         return False
+
     try:
-        put(archive_path, '/tmp/')
+        put(archive_path, "/tmp/")
 
-        archive_filename = archive_path.split('/')[-1]
-        folder_name = archive_filename.split('.')[0]
-        remote_path = "/data/web_static/releases/{}".format(folder_name)
+        archive_filename = archive_path.split('/')[-1][:-4]
 
-        sudo('mkdir -p {}'.format(remote_path))
-        sudo('tar -xzf /tmp/{} -C {}'.format(archive_filename, remote_path))
+        run("mkdir -p /data/web_static/releases/{}/".format(archive_filename))
 
-        sudo('rm /tmp/{}'.format(archive_filename))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_filename + ".tgz", archive_filename))
 
-        sudo('rm -rf /data/web_static/current')
-        sudo('ln -s {} /data/web_static/current'.format(remote_path))
+        run("rm /tmp/{}".format(archive_filename + ".tgz"))
 
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
+            .format(archive_filename, archive_filename))
+
+        run("rm -rf /data/web_static/releases/{}/web_static".format(archive_filename))
+
+        run("rm -rf /data/web_static/current")
+
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(archive_filename))
+
+        print("New version deployed!")
         return True
     except Exception as e:
-        print(e)
         return False
+
+    if __name__ == "__main__":
+    archive_path = do_pack()
+    if archive_path:
+        result = do_deploy(archive_path)
+        if result:
+            exit(0)
+    exit(1)
