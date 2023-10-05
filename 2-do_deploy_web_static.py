7f1315861d21dc89378cfadabc06a08b982f56b3
biitopher <biitopher@gmail.com>
2023-10-05 07:32:38 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,23 +14,20 @@ def do_deploy(archive_path):
     """Distributes an archive to the web servers."""
     if not os.path.exists(archive_path):
         return False
+    filename = archive_path.split('/')[-1]
+
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
 
     try:
-        put(archive_path, '/tmp/')
-
-        archive_filename = os.path.basename(archive_path)
-        folder_name = archive_filename[:-4]
-        run('mkdir -p /data/web_static/releases/{}'.format(folder_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            archive_filename, folder_name))
-
-        run('rm /tmp/{}'.format(archive_filename))
-
-        run('rm -f /data/web_static/current')
-       	run('ln -s /data/web_static/releases/{} /data/web_static/current'\
-        .format(folder_name))
-
-        print("New version deployed!")
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
         return True
 
     except Exception as e:
