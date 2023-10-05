7ce729b143b2d31ccd24c2381a1266abc25b675c
biitopher <biitopher@gmail.com>
2023-10-05 07:44:15 -0700

    Task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -24,7 +24,28 @@ def do_pack():
     else:
         return None
 
+def do_deploy(archive_path):
+    """Distributes an archive to the web servers."""
+    if not os.path.exists(archive_path):
+        return False
+    filename = archive_path.split('/')[-1]
 
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
+
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        return True
+
+    except Exception as e:
+        return False
 
 def deploy():
     """Deploy an archive to the web servers"""
