c57823a1552e1dd8fa393ed2f6716279449084f7
biitopher <biitopher@gmail.com>
2023-10-05 13:27:22 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,33 +14,24 @@ def do_deploy(archive_path):
         return False
 
     try:
-        # Upload the archive to /tmp/ on the server
         put(archive_path, "/tmp/")
 
-        # Get the archive filename without extension
         archive_filename = archive_path.split('/')[-1][:-4]
 
-        # Create the release folder
         run("mkdir -p /data/web_static/releases/{}/".format(archive_filename))
-
-        # Uncompress the archive to the release folder
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(archive_filename + ".tgz", archive_filename))
 
-        # Remove the archive from the server
         run("rm /tmp/{}".format(archive_filename + ".tgz"))
 
-        # Move the contents to the parent folder
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
-            .format(archive_filename, archive_filename))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static
+                /releases/{}/".format(archive_filename, archive_filename))
 
-        # Remove the web_static folder from the release
-        run("rm -rf /data/web_static/releases/{}/web_static".format(archive_filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(
+            archive_filename))
 
-        # Delete the old symbolic link if exists
         run("rm -rf /data/web_static/current")
 
-        # Create a new symbolic link
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(archive_filename))
 
@@ -48,11 +39,3 @@ def do_deploy(archive_path):
         return True
     except Exception as e:
         return False
-
-if __name__ == "__main__":
-    archive_path = do_pack()
-    if archive_path:
-        result = do_deploy(archive_path)
-        if result:
-            exit(0)
-    exit(1)
