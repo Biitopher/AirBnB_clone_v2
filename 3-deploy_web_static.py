0628ff564a550e66288817f296d37b3e7667a714
biitopher <biitopher@gmail.com>
2023-10-05 13:51:49 -0700

    Task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Creates and distributes an archive to your web servers, using function deploy"""
+"""Creates and distributes an archive to your web servers"""
 
 
 from fabric.api import local, put, run, env
