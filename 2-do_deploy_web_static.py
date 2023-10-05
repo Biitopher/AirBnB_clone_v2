2342e955197649d0ad61ca7282a12b28cdc09276
biitopher <biitopher@gmail.com>
2023-10-05 08:18:25 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,28 +7,29 @@ import os
 from datetime import datetime
 
 env.hosts = ['34.229.69.25', '100.26.227.84']
-env.user = 'Ubuntu'
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
     """Distributes an archive to the web servers."""
     if not os.path.exists(archive_path):
         return False
-    filename = archive_path.split('/')[-1]
-
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    tmp = "/tmp/" + filename
-
     try:
-        put(archive_path, "/tmp/")
-        sudo("mkdir -p {}/".format(no_tgz))
-        sudo("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        sudo("rm {}".format(tmp))
-        sudo("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        sudo("rm -rf {}/web_static".format(no_tgz))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s {}/ /data/web_static/current".format(no_tgz))
-        return True
+        put(archive_path, '/tmp/')
 
+        archive_filename = archive_path.split('/')[-1]
+        folder_name = archive_filename.split('.')[0]
+        remote_path = "/data/web_static/releases/{}".format(folder_name)
+
+        sudo('mkdir -p {}'.format(remote_path))
+        sudo('tar -xzf /tmp/{} -C {}'.format(archive_filename, remote_path))
+
+        sudo('rm /tmp/{}'.format(archive_filename))
+
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {} /data/web_static/current'.format(remote_path))
+
+        return True
     except Exception as e:
+        print(e)
         return False
