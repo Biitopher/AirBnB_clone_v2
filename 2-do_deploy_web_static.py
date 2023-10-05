2490d71f347459166cd3a12fe4fb9793a8c75aea
biitopher <biitopher@gmail.com>
2023-10-05 07:13:24 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,24 +13,23 @@ env.user = 'Ubuntu'
 def do_deploy(archive_path):
     """Distributes an archive to the web servers."""
     if not os.path.exists(archive_path):
-         return False
+        return False
 
     try:
         put(archive_path, '/tmp/')
-        
+
         archive_filename = os.path.basename(archive_path)
         folder_name = archive_filename[:-4]
         run('mkdir -p /data/web_static/releases/{}'.format(folder_name))
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
             archive_filename, folder_name))
-        
+
         run('rm /tmp/{}'.format(archive_filename))
-        
+
         run('rm -f /data/web_static/current')
-        
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'.format(
-            folder_name))
-        
+       	run('ln -s /data/web_static/releases/{} /data/web_static/current'\
+        .format(folder_name))
+
         print("New version deployed!")
         return True
 
