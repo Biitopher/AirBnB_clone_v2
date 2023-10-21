c80e240970a01ec25c6ed4ab4b362ca2dca6f4b8
biitopher <biitopher@github.com>
2023-10-21 13:53:03 -0700

    Task 7

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models import storage
 
 
 class State(BaseModel, Base):
