cbe7bd903621fd16070306cf3b2220b8dda5a709
biitopher <biitopher@github.com>
2023-10-21 14:18:35 -0700

    Task 7

--- models/state.py
+++ models/state.py
@@ -22,13 +22,13 @@ class State(BaseModel, Base):
         """Init state"""
         super().__init__(*args, **kwargs)
 
-    if models.storage_t != "db":
+    if models.storage_type != "db":
         @property
         def cities(self):
             """Returns the list of City objects linked to the current State."""
             cities_list = []
-            all_cities = models.storage.all(Cities)
-            for cities in all_cities.values():
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
-                    cities_list.append(cities)
+                    cities_list.append(city)
             return cities_list
