b0412e2d03d238a4d97b6c871e189eecfb9c2fdc
biitopher <biitopher@gmail.com>
2023-10-21 01:16:03 -0700

    Task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script starting flask web applicaton"""
+
+from flask import Flask
+
+app = Flask(__name__)
+def hello_hbnb():
+    """Define a route using strict_slashes"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
