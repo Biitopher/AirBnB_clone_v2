5152c02fb1a3f5bf70c8866abb057dc6491cdc9e
biitopher <biitopher@gmail.com>
2023-10-21 01:20:35 -0700

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,15 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Define a route using strict_slashes"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
