8148ed8fe8e0cb4db61963c41567aaf48d340f6e
biitopher <biitopher@github.com>
2023-10-25 13:01:53 -0700

    Task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template
 from models import *
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
@@ -14,7 +15,7 @@ def states(state_id=None):
     states = storage.all("State")
     if state_id is not None:
         state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
+    return render_template('8-cities_by_states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
